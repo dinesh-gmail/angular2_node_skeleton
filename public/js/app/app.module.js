a6126bb22a37341aeb2573d3377cdc3bde0a9d77
Dinesh Kumar Sahoo <dineshsahoo89@gmail.com>
2017-03-09 09:57:47 +0530

    Created Angular2 Node JS skeleton

---
+++ public/js/app/app.module.js
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from "./app.component";
+export var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule.decorators = [
+        { type: NgModule, args: [{
+                    declarations: [
+                        AppComponent
+                    ],
+                    imports: [BrowserModule],
+                    bootstrap: [AppComponent]
+                },] },
+    ];
+    /** @nocollapse */
+    AppModule.ctorParameters = [];
+    return AppModule;
+}());
+//# sourceMappingURL=app.module.js.map
